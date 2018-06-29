d2558059e0b0e1af73257bf3864c23531fc67d1c
Jason C <jforeza@gmail.com>
2018-06-29 11:21:25 -0700

    Initial commit with project that shows a banner (2 ways) and an interstitial on app launch
    Added the AppNexus Android SDK through gradle (needed to edit both .gradle files)
    Updated proguard-rules.pro to keep required android/google items
    Updated AndroidManifest to include android.permission.ACCESS_FINE_LOCATION and android.permission.ACCESS_COARSE_LOCATION
    Created banana.java with basic functionality to be able to load an ad programatically. Implemented AdListener for Interstitials.
    Updated banana.java to implement all AdListener abstract functions. Not all of them were stated in the documentation.
    Update activity_banana.xml to demonstrate how to show a banner (commented out)

---
+++ app/src/main/java/com/vartyr/appnexus_microwavephone/Banana.java
@@ -0,0 +1,149 @@
+package com.vartyr.appnexus_microwavephone;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.FrameLayout;
+import com.appnexus.opensdk.*;
+import com.appnexus.opensdk.R.*;
+
+public class Banana extends Activity implements AdListener {
+
+    final String logger = "[MICRO]";
+
+    // Set up an ad view with our placement ID.
+    private BannerAdView bav = null;
+    private InterstitialAdView iav = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banana);
+
+        bav = new BannerAdView(this);
+        iav = new InterstitialAdView(this);
+
+        // Configure placement IDs for banner and interstitial
+        bav.setPlacementID("13194659");
+        iav.setPlacementID("13194659"); // 1326299
+
+        Log.v(logger, "Set Placement ID");
+
+
+        // Banner specific configurations
+        bav.setAdSize(320, 50);
+        bav.setAutoRefreshInterval(60000); // Set to 0 to disable auto-refresh
+        bav.setShouldServePSAs(true);
+        bav.setOpensNativeBrowser(true);
+
+        // Interstitial specific configurations
+        iav.setAdListener(this);
+
+
+        // Draw the banner
+        FrameLayout layout = (FrameLayout)findViewById(android.R.id.content);
+        layout.addView(bav);
+
+        // Debug log
+        Log.v(logger, "FrameLayout - Banner added to view");
+
+        // Load / show the ads
+        loadAds();
+
+
+    }
+
+
+    // Method to programatically load the ads
+    public void loadAds() {
+
+        // This handler is needed if the banner does not refresh (ie: setAutoRefreshInterval is set to 0)
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.v(logger, "postDelayed fired, loadAd");
+                bav.loadAd();
+            }
+        }, 0);
+
+
+        iav.loadAd();
+
+
+    }
+
+
+    @Override
+    public void onAdLoaded(AdView av) {
+        Log.v(logger, "onAdLoaded fired, The interstitial ad has loaded");
+        // Now that the ad has loaded, we can show it to the user.
+        InterstitialAdView iav = (InterstitialAdView) av;
+        iav.show();
+
+        // Use this to show an interstitial with a delay instead
+//        iav.showWithAutoDismissDelay(10);
+    }
+
+    @Override
+    public void onAdLoaded(NativeAdResponse av) {
+        Log.v(logger, "onAdLoaded fired, The NativeAd interstitial has loaded");
+    }
+
+    @Override
+    public void onAdRequestFailed(AdView av, ResultCode rc) {
+        Log.v(logger, "onAdRequestFailed fired, Return code ==> " + rc);
+
+    }
+
+    @Override
+    public void onAdClicked(AdView av) {
+        Log.v(logger, "onAdClicked fired, Congrats! " );
+    }
+
+    @Override
+    public void onAdClicked(AdView av, String s) {
+        Log.v(logger, "onAdClicked fired, Congrats! " );
+    }
+
+    @Override
+    public void onAdCollapsed(AdView av) {
+    }
+
+    @Override
+    public void onAdExpanded(AdView av) {
+    }
+
+
+
+
+
+    // Forwarding Lifecycle Callbacks
+    /**
+      * To be called by the developer when the fragment/activity's onDestroy() function is called.
+      */
+    protected void activityOnDestroy(){
+
+     }
+
+    /**
+      * To be called by the developer when the fragment/activity's onPause() function is called.
+      */
+    protected void activityOnPause() {
+
+    }
+    /**
+      * To be called by the developer when the fragment/activity's onResume() function is called.
+      */
+    protected void activityOnResume(){
+
+    }
+
+
+    // TODO: Implement this at a later date
+    protected void checkForPermissions() {
+//        if (ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.WRITE_CALENDAR)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            // Permission is not granted
+//        }
+    }
+}
